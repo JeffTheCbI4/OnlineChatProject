85fd6eb29b4a40edb05b6f09f274ce1e0cbde5e6
Kirill <sergushinkirill@gmail.com>
2021-01-27 17:29:13 +0700

    Добавлена возможность отправки картинок, загрузка листа подключившихся юзеров, плюс мелкие исправления.

--- src/main/java/com/project/chatproject/config/WebSocketConfig.java
+++ src/main/java/com/project/chatproject/config/WebSocketConfig.java
@@ -6,17 +6,23 @@ import org.springframework.messaging.simp.config.MessageBrokerRegistry;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
 import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
+import org.springframework.web.socket.config.annotation.WebSocketTransportRegistration;
 
 @Configuration
 @EnableWebSocketMessageBroker
 public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
     @Override
     public void configureMessageBroker(MessageBrokerRegistry config){
-        config.enableSimpleBroker("/topic");
+        config.enableSimpleBroker("/topic", "/user/queue/specific-user");
         config.setApplicationDestinationPrefixes("/app");
+        config.setUserDestinationPrefix("/user");
     }
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry){
         registry.addEndpoint("/chat-websocket").withSockJS();
+    }
+    public void configureWebSocketTransport(WebSocketTransportRegistration registration) {
+        registration.setMessageSizeLimit(500 * 1024);
+        registration.setSendBufferSizeLimit(1024 * 1024);
     }
 }
--- src/main/java/com/project/chatproject/controller/MessageController.java
+++ src/main/java/com/project/chatproject/controller/MessageController.java
@@ -1,28 +1,43 @@
 package com.project.chatproject.controller;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.project.chatproject.messages.Message;
-import com.project.chatproject.messages.MessageDetails;
+import lombok.Getter;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.messaging.handler.annotation.Header;
 import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.messaging.simp.SimpMessageHeaderAccessor;
+import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.util.HtmlUtils;
 
+import java.util.HashSet;
+import java.util.Set;
+
+@Getter
 @Controller
 public class MessageController {
 
+    private Set<String> userList = new HashSet<>();
+    @Autowired
+    private SimpMessagingTemplate messagingTemplate;
+
     @MessageMapping("/message-details")
     @SendTo("/topic/messages")
-    public Message sendMessage(MessageDetails messageDetails) {
-        return new Message(HtmlUtils.htmlEscape(messageDetails.getUsername()) + ": " +
-                messageDetails.getText());
-    }
-
-    @MessageMapping("/users/connect")
-    @SendTo("/topic/messages")
-    public Message userConnect(MessageDetails messageDetails, SimpMessageHeaderAccessor headerAccessor) {
-        headerAccessor.getSessionAttributes().put("username", messageDetails.getUsername());
-        return new Message("Пользователь " + HtmlUtils.htmlEscape(messageDetails.getUsername())
-                + " присоединился к чату!");
+    public Message sendMessage(@Payload Message message, SimpMessageHeaderAccessor headerAccessor,
+                               @Header("simpSessionId") String sessionId) throws JsonProcessingException {
+        message.setUsername(HtmlUtils.htmlEscape(message.getUsername()));
+        if (message.getType().equals(Message.Type.CONNECT)){
+            headerAccessor.getSessionAttributes().put("username", message.getUsername());
+            ObjectMapper objectMapper = new ObjectMapper();
+            String userListJSON = objectMapper.writeValueAsString(userList);
+            messagingTemplate.convertAndSend("/user/queue/specific-user" + "-user" + sessionId, userListJSON);
+            userList.add(message.getUsername());
+            return message;
+        }
+        return message;
     }
 }
